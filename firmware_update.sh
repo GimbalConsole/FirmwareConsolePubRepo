cce25acabf9775c308211e6c5cf2be0464122eb0
gimbaladmin <gimbaltest123@hotmail.com>
2024-06-30 17:14:13 -0400

    update script for changing the filename

--- firmware_update.sh
+++ firmware_update.sh
@@ -16,7 +16,7 @@ fi
 release_info=$(curl -s https://api.github.com/repos/GimbalConsole/FirmwareConsolePubRepo/releases/latest)
 tag_name=$(echo "$release_info" | grep -o '"tag_name": *"[^"]*"' | cut -d '"' -f 4)
 download_url="https://github.com/GimbalConsole/FirmwareConsolePubRepo/releases/download/$tag_name/FirmwareConsole.bin"
-file_name="GimbalConsole_$tag_name.bin"
+file_name="FirmwareConsole_$tag_name.bin"
 
 echo $tag_name
 echo $download_url
